db2f92107a4ca9927fb3b5cf3c6617ece47f01db
bastola arun <bastolamaka6@gmail.com>
2025-02-08 15:54:07 +0900

    Add project files.

---
+++ DataGridSorting/ViewModel/DataGridSortingViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridSorting.ViewModel
+{
+    public class DataGridSortingViewModel
+    {
+        public ObservableCollection<Person> People { get; set; }
+
+        public DataGridSortingViewModel()
+        {
+            People = new ObservableCollection<Person>
+                    {
+                        new Person { Name = "Arun", Age = 30, Email = "arun@example.com" },
+                        new Person { Name = "John", Age = 25, Email = "john@example.com" },
+                        new Person { Name = "Jane", Age = 28, Email = "jane@example.com" }
+                    };
+        }
+
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string Email { get; set; }
+        }
+
+    }
+}
